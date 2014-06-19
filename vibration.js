fb2bf8d3f5810ee5abd8a99f6cefa9e531989395
lucasholucasho <lho@princeton.edu>
2014-06-19 00:46:43 -0700

    Update vibration.js

--- vibration.js
+++ vibration.js
@@ -1,11 +1,11 @@
 function startTime() {
 	var date = new Date();
 	var hourBin = date.getHours().toString(2);
-	var minutes = date.getMinutes().toString(2);;
+	var minutesBin = date.getMinutes().toString(2);;
 	var seconds = date.getSeconds();
 	simply.setText({
 		title: 'The time in binary',
-		body: hour+":"+minutes+":"+seconds,
+		body: hourBin+":"+minutesBin+":"+seconds,
 		}, true);
 }
 
