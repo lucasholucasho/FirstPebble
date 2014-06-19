61245c81b153b700e5cb6e506d8d9d1db705c1e2
lucasholucasho <lho@princeton.edu>
2014-06-18 23:36:06 -0700

    Update vibration.js

--- vibration.js
+++ vibration.js
@@ -1,22 +1,25 @@
-var date = new Date();
-var hour = date.getHours();
-var minutes = date.getMinutes();
-
 var hourBinary;
 var minutesBinary;
 
-if (parseInt(minutes) >= 10)
-{
-	simply.setText({
-		title: 'The time in binary',
-		body: hour+":"+minutes,
-		}, true);
+function startTime() {
+	var date = new Date();
+	var hour = date.getHours();
+	var minutes = date.getMinutes();
+	if (parseInt(minutes) >= 10)
+	{
+		simply.setText({
+			title: 'The time in binary',
+			body: hour+":"+minutes,
+			}, true);
+	}
+	else
+	{
+		simply.setText({
+			title: 'The time in binary',
+			body: hour+":0"+minutes,
+			}, true);	
+	}
+	var t = setTimeout(function(){startTime()},500);
 }
 
-else
-{
-	simply.setText({
-		title: 'The time in binary',
-		body: hour+":0"+minutes,
-		}, true);	
-}
+startTime();
