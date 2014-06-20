8d597ab0022ac692e724e22c53448e9de1385de6
lucasholucasho <lho@princeton.edu>
2014-06-20 01:19:48 -0700

    Update bartSched.js

--- bartSched.js
+++ bartSched.js
@@ -1,6 +1,6 @@
 ajax({ url: 'http://api.bart.gov/api/sched.aspx?cmd=depart&orig=dubl&dest=powl&time=now&key=ZULP-JR5M-IVKQ-DT35&b=0&a=4' }, function(data){
   var headline = data.match(/<trip(.*?)<\/trip>/);
-  simply.title(headline[0]);  
+  simply.title(headline[1]);  
 });
 
 simply.scrollable(true);
